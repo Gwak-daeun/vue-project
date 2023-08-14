6df7fff1e1e3e2748625bc2eb18352aa0850966b
Gwak-daeun <bbibibbibi22@gmail.com>
2023-08-14 20:13:27 +0900

    todo리스트 부모 자식 프롭스, 컨텍스트 사용

---
+++ src/components/TodoList.vue
@@ -0,0 +1,47 @@
+<template>
+      <div 
+      class="card mt-3"
+      v-for="(todo, index) in todos"
+      :key="todo.id"
+    >
+    <!-- v-for를 사용할 땐 유니크한 키값을 넣어줘야 한다. 엘레멘트(각각의 노드들)를 추적하기 위해 -->
+      <div class="car-body p-2 d-flex align-items-center">
+        <div class="form-check flex-grow-1">
+          <input 
+            class="form-check-input"  
+            type="checkbox" 
+            v-model="todo.completed"
+          />
+          <label 
+            class="form-check-label"
+            :class="{todo: todo.completed}"
+           >
+            {{ todo.subject }}
+          </label>
+        </div>
+        <div>
+            <button 
+              class="btn btn-danger btn-sm"
+              @click="deleteTodo(index)"
+            >
+              Delete
+            </button>
+          </div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props:  {
+        todos: {
+            type: Array,
+            required: true
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/TodoSimpleForm.vue
@@ -0,0 +1,65 @@
+<template>
+      <form 
+      @submit.prevent="onSubmit"
+    >
+    <div class="d-flex">
+      <div class="flex-grow-1 mr-2">
+        <input
+          class="form-control"
+          type="text" 
+          v-model="todo" 
+          placeholder="새로 할 일을 적으세요."
+        />
+      </div>
+      <div>
+        <button 
+          class="btn btn-primary"
+          type="submit"
+        >
+          추가하기
+        </button>
+      </div>
+    </div>
+      <div v-show="hasError" style="color: red;">
+        This field cannot be empty
+      </div>
+    </form>
+</template>
+
+<script>
+import { ref } from 'vue';
+    export default{
+        setup(props, context) { // 데이터를 자식 -> 부모로 보낼 때 필요한게 context에 들어있다.
+            const todo = ref('');
+            const hasError = ref(false);
+            const todos = ref([]);
+
+            const onSubmit = () => {
+     // e.preventDefault(); // 화면 리로딩 방지 @submit.prevent와 동일하다.
+
+            if(todo.value == ''){
+                hasError.value = true;
+            } else {
+                context.emit('add-todo', {
+                    id: Date.now(), 
+                    subject: todo.value, //id는 유니크해야 하기 때문에 타임스탬프로 아이디 생성
+                    completed: false,
+                }); //자식에서 부모로 데이터를 보낸다
+            hasError.value = false;
+            todo.value = '';
+            }
+            };
+
+            return {
+                todo,
+                hasError,
+                onSubmit,
+                todos,
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
